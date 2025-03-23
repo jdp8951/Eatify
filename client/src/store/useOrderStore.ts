5a1ed9c1e816f0a57a2faa49e5d037814ee862b4
Jigar Prajapati <149820949+jdp8951@users.noreply.github.com>
2025-03-23 20:06:44 +0530

    Frontend

---
+++ client/src/store/useOrderStore.ts
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+import { CheckoutSessionRequest, OrderState } from "@/types/orderType";
+
+
+const API_END_POINT: string = "http://localhost:8000/api/v1/order";
+axios.defaults.withCredentials = true;
+
+export const useOrderStore = create<OrderState>()(persist((set => ({
+    loading: false,
+    orders: [],
+    createCheckoutSession: async (checkoutSession: CheckoutSessionRequest) => {
+        try {
+            set({ loading: true });
+            const response = await axios.post(`${API_END_POINT}/checkout/create-checkout-session`, checkoutSession, {
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                withCredentials: true 
+            });
+            // console.log("API Request Sent To:", `${API_END_POINT}/checkout/create-checkout-session`);
+            
+            window.location.href = response.data.session.url;
+            set({ loading: false });
+        } catch (error) {
+            set({ loading: false });
+        }
+    },
+    getOrderDetails: async () => {
+        try {
+            set({loading:true});
+            const response = await axios.get(`${API_END_POINT}/`);
+          
+            set({loading:false, orders:response.data.orders});
+        } catch (error) {
+            set({loading:false});
+        }
+    }
+})), {
+    name: 'order-name',
+    storage: createJSONStorage(() => localStorage)
+}))
